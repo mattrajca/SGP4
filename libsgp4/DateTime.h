4dd45eb87b9ecb9a775cc45c680cd08640ba217b
Matt Rajca <matt.rajca@me.com>
2013-11-11 17:08:36 -0600

    Fixed time handling

--- libsgp4/DateTime.h
+++ libsgp4/DateTime.h
@@ -24,6 +24,9 @@
 #include "TimeSpan.h"
 #include "Util.h"
 
+#include <mach/mach.h>
+#include <mach/clock.h>
+
 namespace
 {
     static int daysInMonth[2][13] = {
@@ -140,8 +143,16 @@ public:
     {
         DateTime dt;
         struct timespec ts;
+		
+		clock_serv_t cclock;
+		mach_timespec_t mts;
+		host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+		kern_return_t ret = clock_get_time(cclock, &mts);
+		mach_port_deallocate(mach_task_self(), cclock);
+		ts.tv_sec = mts.tv_sec;
+		ts.tv_nsec = mts.tv_nsec;
 
-        if (clock_gettime(CLOCK_REALTIME, &ts) == 0)
+        if (ret == 0)
         {
             if (microseconds)
             {
